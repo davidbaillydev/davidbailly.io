80c7fb0540aa59be0164888fea3a23f3d68d5eb9
davidbaillydev <65706353+davidbaillydev@users.noreply.github.com>
2020-09-21 19:39:50 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# davidproject
+portofolio
---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+     
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+      
+  	// SWAP ICON ON CLICK
+	// Source: https://stackoverflow.com/a/34254979/751570
+	$('.dark-toggle').on('click',function(){
+		if ($(this).find('i').text() == 'brightness_4'){
+				$(this).find('i').text('brightness_high');
+		} else {
+				$(this).find('i').text('brightness_4');
+		}
+	});
+	
+	$(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
